fc61935b709e233cc538876c05d16462f8081786
iSAD <bradx2713@gmail.com>
2024-07-04 20:19:00 +0200

    Add files via upload

---
+++ hereToCode.java
@@ -0,0 +1,26 @@
+package secondJavaCode;
+
+public class hereToCode {
+	
+	// "I Started Application Development"
+	public static void main(String[] args) {
+		System.out.println("© iSAD");
+		
+		var x = new hereToCode();
+		x.codeMore();
+		var y = new hereToCode();
+		y.myPrayer();
+
+	}
+	
+	// I am here to be Alexander.
+	public void codeMore() {
+		System.out.println("I was born to be great, I grew up to be greater and will die the greatest!");
+	}
+	
+	//So help me God.
+	public void myPrayer() {
+		var prayer = "Dear God make me live your purpose.";
+		System.out.println(prayer);
+	}
+}
